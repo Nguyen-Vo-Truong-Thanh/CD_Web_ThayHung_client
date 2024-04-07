52573e1620a534bac524b2272ff4c4e598d2e810
HuynhAnhTai0207 <20130388@st.hcmuaf.edu.vn>
2024-04-07 13:07:19 +0700

    sửa mobiephone và chi tiết sản phẩm

--- src/components/DetailProduct/DetailProduct.jsx
+++ src/components/DetailProduct/DetailProduct.jsx
@@ -1,26 +1,101 @@
-import React, {Component, useState} from "react";
-import DetailData from "./DetailData";
+import React from "react";
+import { useParams } from "react-router-dom";
 
-const DetailProduct = () =>{
-        const [detail] = useState();
-        return(
-            <div className={'detail-container'}>
-                <div className={'detail-contant'}>
-                    {
-                        detail.map((x) =>{
-                            return(
-                                <>
-                                    <div className={'detail-info'}>
-                                        <div className={'img-box'}>
-                                            <img src={x.cover} alt={x.name}></img>
-                                        </div>
-                                    </div>
-                                </>
-                            )
-                        })
-                    }
-                </div>
+const DetailProduct = () => {
+  const shopItems = [
+    {
+      id: 1,
+      imageUrl: "../images/shops/shops-1.png",
+      name: "Mapple Earphones",
+      price: 180,
+      discount: 25,
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+      categoryId: 1,
+    },
+    {
+      id: 2,
+      imageUrl: "../images/shops/shops-1.png",
+      name: "Mapple Earphones",
+      price: 180,
+      discount: 25,
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+      categoryId: 2,
+    },
+    {
+      id: 3,
+      imageUrl: "../images/shops/shops-1.png",
+      name: "Mapple Earphones",
+      price: 180,
+      discount: 25,
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+      categoryId: 3,
+    },
+    {
+      id: 4,
+      imageUrl: "../images/shops/shops-1.png",
+      name: "Mapple Earphones",
+      price: 180,
+      discount: 25,
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+      categoryId: 1,
+    },
+    {
+      id: 5,
+      imageUrl: "../images/shops/shops-1.png",
+      name: "Mapple Earphones",
+      price: 180,
+      discount: 25,
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+      categoryId: 3,
+    },
+    {
+      id: 6,
+      imageUrl: "../images/shops/shops-1.png",
+      name: "Mapple Earphones",
+      price: 180,
+      discount: 25,
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+      categoryId: 1,
+    },
+  ];
+
+  const { id } = useParams();
+  const item = shopItems.find((item) => item.id === parseInt(id));
+
+  return (
+    <>
+      <div className="product mtop w-100">
+        <div className="img">
+          <span className="discount">{item.discount}% Off</span>
+          <img src={item.imageUrl} alt="" />
+        </div>
+        <div className="product-details">
+          <h3>{item.name}</h3>
+          <div className="rate">
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+          </div>
+          <div>
+            <h4>${item.price}.00 </h4>
+            <div className="w-100 d-flex justify-content-between">
+              <button type="button" className="btn btn-primary">
+                Thêm vào giỏ hàng
+              </button>
             </div>
-        )
-}
-export default DetailProduct
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DetailProduct;
