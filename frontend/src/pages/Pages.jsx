b8d5e503cce471d835a40eccbd7bff8d9de51fa4
HuynhAnhTai0207 <20130388@st.hcmuaf.edu.vn>
2024-06-01 12:56:16 +0700

    xóa file

--- frontend/src/pages/Pages.jsx
+++ frontend/src/pages/Pages.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 import Home from "../components/MainPage/Home";
-import ShopDiscount from "../components/Discount/ShopDiscount";
-import ShopPhone from "../components/shopPhone/ShopPhone";
-import ShopLaptop from "../components/ShopLaptop/ShopLaptop";
-import ShopAppleWatch from "../components/ShopAppleWatch/ShopAppleWatch";
-import ShopHeadphone from "../components/ShopHeadphone/ShopHeadphone";
 import ShopProductCategory from "../components/PageProduct/ShopProductCategory";
 
 const Pages = ({ productItems, addToCart, CartItem, shopItems }) => {
@@ -12,11 +7,6 @@ const Pages = ({ productItems, addToCart, CartItem, shopItems }) => {
     <>
       <Home CartItem={CartItem} />
       <ShopProductCategory />
-      {/* <ShopDiscount shopItems={shopItems} addToCart={addToCart} />
-      <ShopPhone shopItems={shopItems} addToCart={addToCart} />
-      <ShopLaptop shopItems={shopItems} addToCart={addToCart} />
-       <ShopAppleWatch shopItems={shopItems} addToCart={addToCart} />
-      <ShopHeadphone shopItems={shopItems} addToCart={addToCart} /> */}
       </>
   );
 };
