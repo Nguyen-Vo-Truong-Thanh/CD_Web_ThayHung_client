c98b59ca2014339cc85635b449b369b4a53d3a22
Nguyen-Vo-Truong-Thanh <20130407@st.hcmuaf.edu.vn>
2024-06-04 00:31:35 +0700

    api thêm sửa xóa thông tin sản phẩm

--- backend/src/main/java/vn/edu/hcmuaf/nvtt/backend/services/ProductService.java
+++ backend/src/main/java/vn/edu/hcmuaf/nvtt/backend/services/ProductService.java
@@ -9,6 +9,7 @@ import vn.edu.hcmuaf.nvtt.backend.entity.Product;
 import vn.edu.hcmuaf.nvtt.backend.repository.ProductRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -46,6 +47,41 @@ public class ProductService {
     public Page<Product> getProductByPage(int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         return productRepository.findAllProducts(pageable);
+    }
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+    public void deleteProduct(Long productId) {
+        Optional<Product> product = productRepository.findById(productId);
+        if (product.isPresent()) {
+            productRepository.deleteById(productId);
+        } else {
+            throw new RuntimeException("Product not found with id: " + productId);
+        }
+    }
+    public void deleteProductByName(String name) {
+        Product product = productRepository.findByName(name);
+        if (product != null) {
+            productRepository.deleteByName(name);
+        } else {
+            throw new RuntimeException("Product not found with name: " + name);
+        }
+    }
+    public Product updateProduct(Long productId, Product newProductData) {
+        Optional<Product> optionalProduct = productRepository.findById(productId);
+        if (optionalProduct.isPresent()) {
+            Product existingProduct = optionalProduct.get();
+            existingProduct.setName(newProductData.getName());
+            existingProduct.setDescription(newProductData.getDescription());
+            existingProduct.setPrice(newProductData.getPrice());
+            existingProduct.setDiscount(newProductData.getDiscount());
+            existingProduct.setImageUrl(newProductData.getImageUrl());
+            existingProduct.setStatus(newProductData.getStatus());
+            existingProduct.setCategory(newProductData.getCategory());
+            return productRepository.save(existingProduct);
+        } else {
+            throw new RuntimeException("Product not found with id: " + productId);
+        }
     }
 
 }
